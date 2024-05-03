cc912ed0ad37e6d82265fb680c21b4ccd1b4c833
Justin Small <justinsmall@mac.com>
2024-05-03 22:33:12 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()]
+
+  },  
+})
