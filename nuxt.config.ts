99a06dd9cb1241fd93df4fec5e877b5abf5a1b49
Justin Small <justinsmall@mac.com>
2024-05-09 13:21:49 +0100

    updated nuxt-config and package.json after moving stuff over from tailwind-nuxt-clean

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -8,5 +9,17 @@ export default defineNuxtConfig({
     build: {
       cssCodeSplit: false
     }
+  },  
+  modules: ['nuxt-icon', '@vueuse/nuxt', "@nuxt/fonts", "nuxt-primevue"],
+  primevue: {
+    // cssLayerOrder: 'base, components, primevue, utilities',
+    components: {
+      include: '*',
+      exclude: ['Card', 'Carousel', 'Editor', 'Chart']
+    },    
+    options: {
+      unstyled: true,      
+    },
+    importPT: { as: 'Lara', from: path.resolve(__dirname, './prime-vue/presets/lara') },
   },  
 })
